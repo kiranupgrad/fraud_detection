3b1618215a0c6b25970a15aad4618551899a067e
Chakkilam Kiran <chakkilam.kiran@mavenir.com>
2024-02-24 13:59:43 +0530

    model branch changes

--- data_proessing.py
+++ data_proessing.py
@@ -3,8 +3,3 @@ print("This is second line to commit")
 a = 13
 b = 14
 
-def sum(a,b):
-    sum = a+b
-    return a
-
-print("This is invalid sum return commit")
---
+++ model.py
@@ -0,0 +1,1 @@
+print("This is for model branch commit")
